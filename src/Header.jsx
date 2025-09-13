ba6641ec65bfe2444d653a5164f53044e3c1da32
Yusupova Muxlisa <umuhlisa716@gmail.com>
2025-09-13 14:02:32 +0500

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
       <section className="py-4">
         <div className="w-full px-4 sm:px-6 md:px-10 lg:px-[90px]">
           <div className="flex flex-col sm:flex-row justify-between items-center flex-wrap gap-4">
-            {/* Логотип */}
+{/*Logo */}
             <div className="flex items-center space-x-2">
               <div className="w-12 h-12 bg-blue-500 rounded-full flex items-center justify-center relative">
                 <div className="absolute top-1">
@@ -21,7 +21,7 @@ const Header = () => {
               </h1>
             </div>
 
-            {/* Кнопка */}
+            {/* Contact_button */}
             <button className="p-2 px-4 rounded bg-blue-500 text-white text-sm sm:text-base hover:bg-blue-600 transition">
               <a href="#kontakt">Kontaktlar</a>
             </button>
